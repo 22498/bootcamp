f5fe42f74042f52f6e168d9227a409c6185c9765
22498 <22498@ma-web.nl>
2017-09-15 12:39:09 +0200

    Add files via upload

---
+++ bootcamp.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <script>
+        var color;
+        color=orange;
+
+        function declareVariables() {
+            var object1="bag";
+            var object2="car";
+            var object3="house";
+            var object4="dog";
+            var object5="cat";
+            var object6="mouse";
+            var object7="chair";
+            var object8="table";
+            var object9="floor";
+            var object10="food";
+            var object11="hotel";
+            var object12="door";
+            var object13="window";
+            var object14="bus";
+
+            var Number1="9";
+            var Number2="10";
+            var Number3="11";
+            var goLeft=true;
+            var goRight=false;
+        }
+        declareVariables()
+
+
+    </script>
+</head>
+<body>
+
+</body>
+</html>
